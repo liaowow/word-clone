940b573f2178761d1556793d38f22c75bccd146d
Annie Liao <liaowow@gmail.com>
2023-03-04 13:37:15 -0500

    Add GuessResults component and render in Game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,7 +11,12 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <GuessInput />;
+  return (
+    <>
+      <GuessResults />
+      <GuessInput />
+    </>
+  );
 }
 
 export default Game;
